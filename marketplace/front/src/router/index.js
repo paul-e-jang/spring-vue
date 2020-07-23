8f74697488d43a59d491998b8d7159a41fb68485
bashpound <eunhackjang@gmail.com>
2020-07-23 15:28:30 +0900

    ESlint complaint
    modified

--- marketplace/front/src/router/index.js
+++ marketplace/front/src/router/index.js
@@ -3,14 +3,13 @@ import VueRouter from 'vue-router'
 import LoginPage from '@/views/LoginPage'
 import RegisterPage from '@/views/RegisterPage'
 
-
 Vue.use(VueRouter)
 
 const routes = [{
   path: '/login',
   name: 'loginPage',
   component: LoginPage
-},{
+}, {
   path: '/register',
   name: 'RegisterPage',
   component: RegisterPage
