efdbb988b6277c25330712ccf165039a81da53a6
bashpound <eunhackjang@gmail.com>
2020-07-23 16:51:20 +0900

    Revert "default setting"
    This reverts commit f90d141a10545489f06ad99422bb669f02df1fbb.

--- marketplace/front/src/services/registration/__mocks__/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  register (detail) {
-    return new Promise((resolve, reject) => {
-      detail.emailAddress === 'sunny@taskagile.com'
-        ? resolve({result: 'success'})
-        : reject(new Error('User already exist'))
-    })
-  }
-}
