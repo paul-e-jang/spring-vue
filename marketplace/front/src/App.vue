e7781be2b3aa92ec242455717b652a754bab1635
bashpound <eunhackjang@gmail.com>
2020-07-29 14:25:34 +0900

    footer

--- marketplace/front/src/App.vue
+++ marketplace/front/src/App.vue
@@ -4,7 +4,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,300;1,400;1,500&display=swap" rel="stylesheet">
     <div id="header-wrapper"><Header /></div>
     <div id="router-wrapper"><router-view/></div>
-    <div id="footer-wrapper" class="col-12"><Footer /></div>
+    <div id="footer-wrapper" class="col-12 border-top"><Footer /></div>
   </div>
 </template>
 
