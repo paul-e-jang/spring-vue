aa0c997468efeea9697afcf22ef3583312d420b2
bashpound <eunhackjang@gmail.com>
2020-08-04 23:05:08 +0900

    lombok, authentication

--- marketplace/src/main/java/bashpound/marketplace/web/apis/authenticate/CustomUserDetailsService.java
+++ marketplace/src/main/java/bashpound/marketplace/web/apis/authenticate/CustomUserDetailsService.java
@@ -1,11 +1,18 @@
 package bashpound.marketplace.web.apis.authenticate;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import bashpound.marketplace.domain.common.model.Role;
 import bashpound.marketplace.domain.model.user.SimpleUser;
 import bashpound.marketplace.domain.model.user.UserRepository;
 import bashpound.marketplace.domain.model.user.Users;
@@ -17,7 +24,17 @@ public class CustomUserDetailsService implements UserDetailsService{
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Users user = userRepository.findByUsername(username);
+		
+		Optional<Users> userEntityWrapper = Optional.ofNullable(userRepository.findByUsername(username));
+		Users user = userEntityWrapper.get();
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		if(("eunhackjang").equals(username)) {
+			authorities.add(new SimpleGrantedAuthority(Role.ADMIN.getValue()));
+		}else {
+			authorities.add(new SimpleGrantedAuthority(Role.MEMBER.getValue()));
+		}
 		return new SimpleUser(user);
 	}
 
