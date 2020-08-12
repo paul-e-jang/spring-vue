44f7bd42caab9406fd4ccf0ae771464f8d21a2f4
bashpound <eunhackjang@gmail.com>
2020-08-12 17:57:14 +0900

    pageView

--- marketplace/front/src/views/BoardView.vue
+++ marketplace/front/src/views/BoardView.vue
@@ -1,7 +1,9 @@
 <template>
-  <v-card class="col-6 mx-auto border" id="boardview">
+<div class="row">
+  <div class="col-6">
+  <v-card class="mx-auto border" id="boardview" outlined>
     <v-card-title>
-      Default 게시판 <font color="red">[DEBUG] parceTest: {{ parceTest }} </font>
+      Default 게시판 <font color="red">[DEBUG] Test: {{ parceTest }} </font>
       <v-spacer></v-spacer>
       <v-text-field
         v-model="search"
@@ -12,12 +14,12 @@
       ></v-text-field>
     </v-card-title>
     <v-data-table :items="articles" :search="search" :sort-by="['created_date']"
-    calculated-widths="true" :headers="headers" :loading="loading">
+    calculated-widths="true" :headers="headers" :loading="loading" :dense="dense" class="px-4">
     <template v-slot:body="{ items }">
          <tbody>
           <tr v-for="item in items" :key="item.no">
             <td>{{ item.no }}</td>
-            <td class="text-left">{{ item.subject }}</td>
+            <td class="text-left" @click.prevent="selectArticle(item.no)">{{ item.subject }}</td>
             <td>{{ item.author }}</td>
             <td>{{ item.createdDate }}</td>
             <td>{{ item.viewed }}</td>
@@ -29,6 +31,24 @@
       </template>
     </v-data-table>
   </v-card>
+</div>
+<div class="col-6">
+<v-card class="mx-auto border" id="boardview">
+   [DEBUG] Seleted: {{ selected }}
+   <v-divider />
+    <v-card-title class="text-left">
+      default | {{ selected }}
+      <v-subheader> 작성자: author | 조회: viewed | 댓글: int 작성일: createdDate </v-subheader>
+    </v-card-title>
+    <v-divider />
+    <p class="text-left" v-html="viewcontent">  </p>
+    <v-divider />
+    <div class="text-right mx-3">수정 | 삭제 | 글쓰기</div>
+    <v-divider />
+    <div class="text-left border px-3 mx-3"> 댓글 </div>
+  </v-card>
+  </div>
+   </div>
 </template>
 
 <script>
@@ -60,15 +80,21 @@ export default {
           this.articles[i].createdDate = DateParser.ParceRefactor(this.articles[i].createdDate)
         }
       })
+    },
+    selectArticle (key) {
+      this.selected = key - 1
+      this.viewcontent = this.articles[this.selected].content
     }
   },
   data () {
     return {
+      selected: '',
       search: '',
       height: 800,
       dense: true,
       loading: true,
       timestamp: new Date(),
+      viewcontent: '',
       time: '2020-08-10T03:39:33.874+00:00',
       headers: [
         {
@@ -82,7 +108,15 @@ export default {
         { text: '작성일', value: 'createdDate', sortable: false, width: '10%' },
         { text: '조회', value: 'viewed', sortable: false, width: '10%' }
       ],
-      articles: []
+      articles: [],
+      desserts: [
+        {
+          no: '1',
+          subject: 'Frozen Yogurt',
+          viewed: 159,
+          content: '<br>tag applied<br>something<br>lorem'
+        }
+      ]
     }
   }
 }
@@ -91,5 +125,8 @@ export default {
 <style scoped>
 #boardview {
   min-height: 800px;
+}
+#article-content{
+  min-height:500px;
 }
 </style>
